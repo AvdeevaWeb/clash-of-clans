5d5287d5de85e4b446b91451fa7e9c012a645e81
avdeeva <avdeeva_5681@mail.ru>
2023-11-26 12:29:33 +0300

    2

---
+++ src/UI/ui-card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card">
+        <span class="card-name">{{name}}</span>
+        <span class="card-title">{{ title }}</span>
+        <span class="card-body">
+            <slot>
+            </slot>
+        </span>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        name:{
+            type: Number,
+            required: true
+        },
+        title:{
+            type: String,
+            required: true
+        },
+
+    },
+    data(){
+        return{
+        }
+    }
+}
+</script>
--- src/pages/c-home.vue
+++ src/pages/c-home.vue
@@ -1,3 +1,41 @@
 <template>
-    <div><h1>Home page</h1></div>
+    <div>
+        <h1>Home page</h1>
+        <div class="card__wrapper" v-for="item in items" 
+        :key="item.id">
+            <cardUI 
+            :name="item.lvl"
+            :title="item.title">
+            {{ item.descriptoin }}
+            </cardUI>
+        </div>
+
+    </div>
 </template>
+
+<script>
+import cardUI from '@/UI/ui-card.vue'
+export default {
+    components:{
+        cardUI
+    },
+    data(){
+        return{
+            items: [
+                {
+                    id: 1,
+                    title: 'Archer',
+                    descriptoin: 'Archer Archer Archer ',
+                    lvl: 3
+                },
+                {
+                    id: 2,
+                    title: 'Wizard',
+                    descriptoin: 'Wizard Wizard Wizard ',
+                    lvl: 5
+                }
+            ]
+        }
+    }
+}
+</script>
